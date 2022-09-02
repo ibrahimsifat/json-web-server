71dec6a39b6adcfc24dc30537fef3331ad7f848d
Ibrahim Sifat <ibsifat24@gmail.com>
2022-09-02 12:02:01 +0300

    setyp

---
+++ db.js
@@ -0,0 +1,12 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const tags = require("./data/tags");
+// const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  tags,
+  transactions,
+};
